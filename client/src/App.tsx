e236b4dacde45a0d80cd38d557169780a2afcb1d
Maxime TRICOCHE <maxime.trche@gmail.com>
2024-11-14 19:03:32 +0100

    [FEAT] create navbar component and routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,22 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
-  return <div>{/* App */}</div>;
+  return (
+    <div className="h-screen flex flex-col">
+      {/* Le composant Menu viendra ici */}
+      <header>
+        <Navbar />
+      </header>
+      <main className="flex-1 overflow-y-auto ">
+        <div>
+          <Outlet />
+        </div>
+      </main>
+      <footer>{/* Le composant Player viendra ici */}</footer>
+    </div>
+  );
 }
 
 export default App;
